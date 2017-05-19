63e8249511bf2ebc80f794609d8e0841f2bfa66c
Park Donghyeon <emma0415@naver.com>
2017-05-20 03:26:10 +0900

    adc_init() 수정. adc2str 함수 추가

--- main.c
+++ main.c
@@ -6,35 +6,28 @@
 #include "stm32f10x_tim.h"
 #include "adc-dma.h"
 
-vu16 adcValue[4] = {};
-unsigned char buf[21] = {};
+vu16 adcValue[4] = { };
+unsigned char buf[21] = {
+		0, 0, 0, 0, ',',
+		0, 0, 0, 0, ',',
+		0, 0, 0, 0, ',',
+		0, 0, 0, 0, '\r', '\n' };
 
 void TIM2_IRQHandler(void) {
 	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-	static u16 cnt = 0;
-	GPIOA->ODR ^= GPIO_Pin_11;
-	if(cnt++ > 250){
-		cnt = 0;
 
-		int2str(&buf[0], adcValue[0]);
-		int2str(&buf[5], adcValue[1]);
-		int2str(&buf[10], adcValue[2]);
-		int2str(&buf[15], adcValue[3]);
-		UARTSend(buf, 21);
-	}
+	adc2str(buf, adcValue);
+	GPIOA->ODR ^= GPIO_Pin_11;
+	UARTSend(buf, 21);
+	GPIOA->ODR ^= GPIO_Pin_11;
+
 }
 
 int main(void) {
 	usart_init();
 	gpio_init();
 	timer_init();
-	buf[4] = ',';
-	buf[9] = ',';
-	buf[14] = ',';
-	buf[19] = '\r';
-	buf[20] = '\n';
 	adc_init();
-
 
 	while (1) {
 
